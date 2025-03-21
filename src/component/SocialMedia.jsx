d7d5924e23d981b8e4ff76cbb728afe63d6dffd7
bimela tony <tonybimela@gmail.com>
2025-03-22 00:25:29 +0100

    made changes

--- src/component/SocialMedia.jsx
+++ src/component/SocialMedia.jsx
@@ -2,13 +2,13 @@ export default function SocialMedia() {
   return (
     <>
       <div className="media-accounts">
-        <img src="/public/images/Vector.png" />
+        <img src="/images/Vector.png" />
 
-        <img src="/public/images/twitter.png" />
+        <img src="/images/twitter.png" />
 
-        <img src="/public/images/in.png" />
+        <img src="/images/in.png" />
 
-        <img src="/public/images/ig.png" />
+        <img src="/images/ig.png" />
       </div>
     </>
   );
