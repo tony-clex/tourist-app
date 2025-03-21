0d6caa36d7b63aabfee61dcc7f9cad25e8f2d705
bimela tony <tonybimela@gmail.com>
2025-03-21 23:52:52 +0100

    made changes

--- src/constant.js
+++ src/constant.js
@@ -2,7 +2,7 @@
 export const HomecardItems = [
   {
   
-    src: "/public/images/giraf.png",
+    src: "https://s3-alpha-sig.figma.com/img/6521/b928/1e965b9e2b4c3f113bb67ce649d7a461?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=uBaK6vMN1640xbOvsn4mjSv8XZRlol5USFIjbuhktCtxrWTERxnKW3O2AoaFk5qglWzy5WkO5DRI-7sqfkhtYCxUFqM0faIrCGKViC72USIvaauLpDoQtyHXzEPiGxBL-uj5DWA4b4HoqUP468gd~R-MvZiAcxDkOG7hFnd2CndEXpDHqbyOpZTXJ~If6wvlKhhHP6VLNDEhP7uGnY959-cnp~BXTDtCH~1mxdgTDBxK3deS44ohgR88rvf~Rbj1C~pPz0WYWoGSvMIXPiehILBszOsDcGvJwIA4SLMpA8J~VgdWxBLYAMznA0-HLMrU4uruQyQTSFouO51K5LaKzA__",
     title: "Akagera National Park",
     description:
       "Visitors to Akagera National Park can take part in a range of activities, including game drives, boat safaris, bird watching, and nature walks.",
@@ -10,14 +10,14 @@ export const HomecardItems = [
   },
 
   {
-    src: "/public/images/bio.png",
+    src: "https://s3-alpha-sig.figma.com/img/5d06/b58d/ba0e694cee79ef5384b5687366cc3ed3?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=lSPWZKzmzYIW39E3qffqNGcl~zkboPmWT2XSRD~WvmR3ttcHqF9w4rEgDVHlUIvMsydIyeEUXpQURJJYv7mwhKyMHQ1fBcBr2xkbrO-zmfBBNoYThmyxJRON7jkqRr6xCPiRQDHtV6HX3Mp84IptzeoxblVkbUcX8TvQ79mJ8~VPq47WsjYBZuo8q2Gwy5yt96O9fDxTOJKB4nrpShPX6ulDH6Tw7UvL~hb51uuIctKaog90OBvobQAVRcHF43CkX0z~pn1HDAKb7MDyKCjqSQH-I9HsviRSW0s3iWg2~sn3ydSWkvkiiYzIzO8sTC-v7os8eEOqokX5ckLKGNI9HA__",
     title: "Bisoke Hike",
     description:
       "The Bisoke hike is a popular mountain trek in Rwanda, located in the Volcanoes National Park. It is one of the most challenging hikes in the park.",
   },
 
   {
-    src: "/public/images/lake.png",
+    src: "https://s3-alpha-sig.figma.com/img/2509/3e3b/45eb67d244449dc32bcd471b5af02dde?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=jwhJ0j7M3qiEnfxWze5zaHHxijC7OBQiJldRh~LWdiyXiEhyBj1GDrZ8hL2JqNFZDasaBqGSc2OGCfBtqVH3E1qmJPd6zuih5vQlds7WfyerupYbDd7nU1bm~He~QiMAzFZmA8txFvu4cUXEq~w3aeYn3tsYgeH170PoHykDb27OueMqc65A1xq~IvL4YQ~JLO-nSBRruuSNcEfHLefJUEEM5prPT1tmDeqa3LxBmPHJPQr0mdJ0pN0ahzQbLwOSpZMWPCPbSYLiNtB1qoSCaP2D2oXz4hws3YahUTpVF2VAETIybMB0XiYNtHNlOyJfp5NcXF4ARRtagpRkRcHRnQ__",
     title: "Lake Kivu trip",
     description:
       "A trip to Lake Kivu can be a truly unique and memorable experience.  sxpirience is one of the most challenging and jdd8djiehi gvdqekjgeiwhsutw  kjgsw",
