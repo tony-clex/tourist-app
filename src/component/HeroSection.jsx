1c29a5ac81e216e98a20734031bdd9c60f761cf9
bimela tony <tonybimela@gmail.com>
2025-03-21 23:39:06 +0100

    made changes

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -3,7 +3,7 @@ export default function HeroSection() {
     <>
       <div className="hero-section">
         <img
-          src="/public/images/Image Placeholder (25).png"
+          src="https://s3-alpha-sig.figma.com/img/673f/2e91/1bcc8157232409198c246b045a73b73f?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=IgjaUzs-72B188Gu9YnMV7P6T436H9XDQRM8hM28kzYV0-FzfGgt3VD3ZQMoLbk92hSlBTWE7pUz6nERcQKTwQKNmDtOcfOkaOUuhs7AmHfLSnmpE6Er2Yo2N53tKGYCYCeack1oSFpQB32GUqPcPEKEV2Jnh6EvYjpURB5ixg7jiFPGxcXYE0qN5eWVZikE2AGXkk74zW1PKBgFK9kZfnKeY88kdmhvbHLzc2R0BGez82dzxUiaEKr~RkQr1oT2rDg~Xvn-i1o7EnqPkxsvYlLL5-F6X72sLqteSxBSA91x2YxINmHSlsB2Md3NTaYS7G7m7HpsqHZwacuuge~HJw__g"
           alt="island"
         />
       </div>
